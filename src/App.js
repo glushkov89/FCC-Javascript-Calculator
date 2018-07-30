ea4b9578d8d18002c1509a086f36c0b22eed5704
glushkov89 <kellerman117@gmail.com>
2018-07-30 09:53:50 +0300

    App.js finished.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import Calculator from "./calculator";
 
 class App extends Component {
 	render() {
-		return <div className="App">Hello Calculator</div>;
+		return <Calculator />;
 	}
 }
 
