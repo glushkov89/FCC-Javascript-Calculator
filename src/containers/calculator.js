adb44fc010c3a0bda0a8495185677b24bf0978ee
glushkov89 <kellerman117@gmail.com>
2018-08-02 17:27:15 +0300

    Finished alert.

--- src/containers/calculator.js
+++ src/containers/calculator.js
@@ -46,13 +46,7 @@ import Alert from "react-s-alert";
 
 //Styles for Alert
 import "react-s-alert/dist/s-alert-default.css";
-import "react-s-alert/dist/s-alert-css-effects/slide.css";
-import "react-s-alert/dist/s-alert-css-effects/scale.css";
 import "react-s-alert/dist/s-alert-css-effects/bouncyflip.css";
-import "react-s-alert/dist/s-alert-css-effects/flip.css";
-import "react-s-alert/dist/s-alert-css-effects/genie.css";
-import "react-s-alert/dist/s-alert-css-effects/jelly.css";
-import "react-s-alert/dist/s-alert-css-effects/stackslide.css";
 
 //Components
 import Processor from "./processor";
